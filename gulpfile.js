069427851cd0791a178a56153973bd67cd9172c0
mariusz89016 <mariusz89016@gmail.com>
2016-03-01 16:32:11 +0100

    Splitted tasks & added watch

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,19 @@ const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync');
 
 
-gulp.task('clean', function () {
-    return del('dist/**/*');
+gulp.task('clean:libs', function () {
+    return del('dist/lib');
 });
 
-gulp.task('compile', function(){
+gulp.task('clean:assets', function() {
+    return del('dist/**/*.html');
+});
+
+gulp.task('clean:compiled', function() {
+  return del('dist/app');
+});
+
+gulp.task('compile', ['clean:compiled'], function(){
     gulp.src(['app/**/*.ts'])
         .pipe(sourcemaps.init())
         .pipe(typescript(tscConfig.compilerOptions))
@@ -18,7 +26,7 @@ gulp.task('compile', function(){
         .pipe(gulp.dest('dist/app'));
 });
 
-gulp.task('copy:libs', ['clean'], function() {
+gulp.task('copy:libs', ['clean:libs'], function() {
     return gulp.src([
             'node_modules/angular2/bundles/angular2-polyfills.js',
             'node_modules/systemjs/dist/system.src.js',
@@ -30,17 +38,22 @@ gulp.task('copy:libs', ['clean'], function() {
         .pipe(gulp.dest('dist/lib'))
 });
 
-gulp.task('copy:assets', ['clean'], function() {
+gulp.task('copy:assets', ['clean:assets'], function() {
     return gulp.src(['index.html'], { base : './' })
         .pipe(gulp.dest('dist'))
 });
 
-gulp.task('serve', ['compile', 'copy:libs','copy:assets'], function() {
+gulp.task('copy', ['copy:assets', 'copy:libs']);
+
+gulp.task('serve', ['compile', 'copy'], function() {
     browserSync({
         server: {
             baseDir: 'dist'
         }
     });
+
+    gulp.watch(assets, ['copy:assets', browserSync.reload]);
+    gulp.watch('app/**/*', ['compile', browserSync.reload]);
 });
 
 gulp.task('default', ['serve']);
