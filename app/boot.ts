613e38f68db4e5e2bcd02c0e71355acedb61beb6
DeJw92 <dpawlak1992@gmail.com>
2016-03-03 11:34:20 +0100

    HashLocationStrategy added

--- app/boot.ts
+++ app/boot.ts
@@ -2,5 +2,7 @@
 import {bootstrap} from 'angular2/platform/browser'
 import {App} from "./app";
 import {ROUTER_PROVIDERS} from "angular2/router";
+import {LocationStrategy, HashLocationStrategy} from "angular2/router";
+import {provide} from "angular2/core";
 
-bootstrap(App,[ROUTER_PROVIDERS]);
+bootstrap(App,[ROUTER_PROVIDERS,provide(LocationStrategy, {useClass: HashLocationStrategy})]);
