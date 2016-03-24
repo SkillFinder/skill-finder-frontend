4f37097545f4b426f2d655ce191825ae5378e235
DeJw92 <dpawlak1992@gmail.com>
2016-03-24 21:48:33 +0100

    custom form builder added

--- app/login-form.ts
+++ app/login-form.ts
@@ -1,9 +1,28 @@
 import {Component} from "angular2/core";
+import {FORM_DIRECTIVES} from "angular2/common";
+import {FormBuilder} from "angular2/common";
+import {ControlGroup} from "angular2/common";
+import {Validators} from "angular2/common";
 
 @Component({
     selector: 'login-form',
-    templateUrl: 'views/login-form.html'
+    templateUrl: 'views/login-form.html',
+    directives: [FORM_DIRECTIVES]
 })
 export class LoginFormComponent {
+
+    loginForm: ControlGroup;
+
+    constructor(formBuilder: FormBuilder) {
+        this.loginForm = formBuilder.group({
+            "login": ["", Validators.required],
+            "password":["", Validators.required]
+        })
+    }
+
+
+    onSubmit():void {
+        console.log(this.loginForm);
+    }
 
 }
